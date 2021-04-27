0984add2894c1e16f6fe819c1dbd247c575d4190
Andy Tan <18187444+tanandy@users.noreply.github.com>
2021-04-27 15:41:03 +0200

    Discover tags

--- CreateJobs.groovy
+++ CreateJobs.groovy
@@ -29,18 +29,51 @@ repositories.each {
 
   multibranchPipelineJob(repo) {
     branchSources {
-      github {
-        id(repo)
-        scanCredentialsId('github')
-        repoOwner('OpenPaaS-Suite')
-        repository(repo)
-        buildOriginPRHead(true)
-        buildOriginBranchWithPR(false)
-        traits {
-          gitBranchDiscovery()
-          gitTagDiscovery()
+        branchSource {
+            source {
+                github {
+                    id(repo)
+                    credentialsId('github')
+                    repoOwner('OpenPaaS-Suite')
+                    repository(repo)
+                    repositoryUrl('')
+                    configuredByUrl(false)
+                    traits {
+                        gitHubTagDiscovery()
+                        gitHubBranchDiscovery { strategyId(1) }
+                        gitHubPullRequestDiscovery { strategyId(1) }
+                    }
+                }
+            }
+            buildStrategies {
+                buildAnyBranches {
+                    buildChangeRequests {
+                        ignoreTargetOnlyChanges(true)
+                        ignoreUntrustedChanges(false)
+                    }
+                    buildNamedBranches {
+                        filters {
+                            exact {
+                                name('master')
+                                caseSensitive(true)
+                            }
+                        }
+                    }
+                    buildTags {
+                        atLeastDays '-1'
+                        atMostDays '14'
+                    }
+                }
+            }
         }
-      }
+    }
+    configure {
+        // workaround for JENKINS-46202 (https://issues.jenkins-ci.org/browse/JENKINS-46202)
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait' {
+            strategyId 1
+            trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustPermission')
+        }
     }
     triggers {
       periodicFolderTrigger {
